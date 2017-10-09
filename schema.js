e58e561086e30a125202cd83c7f89237429908f2
Jeffrey Degoma <jeff@Jeffreys-MBP.fios-router.home>
2017-10-09 11:24:35 -0400

    Added schema and CRUD resolvers

--- schema.js
+++ schema.js
@@ -1,13 +1,21 @@
 export default `
     
     type Query {
-        hello: String!
+        allUsers: [User!]!
+        getUser(username: String!): User
     }
 
     type User {
+        id: Int!
         username: String!
+        createdAt: String!
+        updatedAt: String!
     }
 
-   
+    type Mutation {
+        createUser(username: String!): User
+        updateUser(username: String!): [Int!]!
+        deleteUser(username: String!): Int!
+    }
 
 `
