b23b7964e16982d0e8b9bcf109b01092960822a1
Jeffrey Degoma <jeff@Jeffreys-MBP.fios-router.home>
2017-10-10 10:23:06 -0400

    Created Associations with sequelize

--- models/index.js
+++ models/index.js
@@ -7,22 +7,24 @@ const sequelize = new Sequelize(
   'test_graphql_admin', 
   'iamapassword', 
 
-  {
+  {  
   host: 'localhost',
   dialect: 'postgres',
   },
 );
 
 const db = {
-  User: sequelize.import('./user')
+  User: sequelize.import('./user'),
+  Board: sequelize.import('./board'),
+  Suggestion: sequelize.import('./suggestion')
 }
 
 
-// Object.keys(db).forEach(function(modelName) {
-//   if ("associate" in db[modelName]) {
-//     db[modelName].associate(db);
-//   }
-// });
+Object.keys(db).forEach(function(modelName) {
+   if ("associate" in db[modelName]) {
+    db[modelName].associate(db);
+  }
+});
 
 db.sequelize = sequelize;
 // db.Sequelize = Sequelize;
--- models/user.js
+++ models/user.js
@@ -1,8 +1,19 @@
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
-    username: DataTypes.STRING
-  });
+  const User = sequelize.define("User", 
+      {
+        username: DataTypes.STRING,
+      },
+  );
 
+  User.associate = (models) => {
+    User.hasMany(models.Board, {
+        foreignKey: "owner",
+    })
+
+    User.hasMany(models.Suggestion, {
+        foreignKey: "creatorId" 
+    })
+  }
 
   return User;
 };
