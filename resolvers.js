27bc125da843c6428dd01bbf5584921a249e0bcd
Jeffrey Degoma <jeff@Jeffreys-MBP.fios-router.home>
2017-10-12 12:57:06 -0400

    Nested types

--- resolvers.js
+++ resolvers.js
@@ -6,13 +6,30 @@ export default {
                 where: {
                     username,
                 }
-            }),
+        }),
+        userBoards: (parent, { owner }, { models }) => 
+            models.Boards.findAll({
+                where:{
+                    username,
+                }
+        }),
+        userSuggestions: (parent, { creatorId }, { models }) => 
+            models.Suggestion.findAll({
+                where:{
+                    creatorId,
+                }
+        }),
+
     },
 
     Mutation: {
         createUser: (parent, args, { models }) => models.User.create(args),
-        updateUser: (parent, args, { models }) => models.User.update(args),
+        updateUser: (parent, { username, newUsername }, { models }) =>
+            models.User.update({ username, newUsername }, {where: { username } }),
         deleteUser: (parent, args, { models }) => 
             models.User.destroy({where: args}),
+        createBoard: (parent, args, { models }) => models.Board.create(args),
+        createSuggestion: (parent, args, { models }) =>
+            models.Suggestion.create(args)
     }
 }
